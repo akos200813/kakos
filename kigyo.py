661d76e999ab898510d1920db19e7ae27ecf7477
akos200813 <akos.attila.kalmar@umszki.hu>
2025-02-05 08:29:00 +0100

    uristen kigyoznak.py

--- kigyo.py
+++ kigyo.py
@@ -19,14 +19,3 @@ def parosossz(a:int):
         if i % 2 == 0:
             osszeg += i
     return osszeg
-
-a = int(input('Adj egy számot: '))
-
-'''szam1 = int(input('Adj egy számot: '))
-szam2 = int(input('Adj egy számot: '))
-szam3 = int(input('Adj egy számot: '))
-
-print(f'A háromszög({szam1} cm, {szam2} cm, {szam3} cm) szerkeszthető? : {szerkesztheto(szam1, szam2, szam3)}\nA kerülete: {kerulet(szam1, szam2, szam3)} cm\nTerülete: {terulet(szam1, szam2, szam3)} cm^2')'''
-
-
-print(f'A páros számok összege {a}-ig: {parosossz(a)}')
